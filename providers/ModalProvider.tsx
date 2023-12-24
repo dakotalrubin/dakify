3a883b83226e9ab98283b75a032c1f2295b12db4
dlrubin <dlrubin@ucsc.edu>
2023-12-24 10:53:44 -0800

    Create a Modal component with hard-coded props for testing

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,41 @@
+// Required for client component to use React hooks
+"use client";
+
+import { useState, useEffect } from "react";
+
+import Modal from "@/components/Modal";
+
+// ModalProvider component only renders client-side modals
+const ModalProvider = () => {
+  // Create a state variable to track whether a modal has been mounted
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Server-side rendering can cause "hydration" errors with modals.
+  // HTML rendered by the server needs to find matching client-side HTML nodes.
+  // To sidestep this, only render a modal from the client side: if useEffect
+  // block runs, then we're on the client side and can safely render a modal.
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  // If something is going to be rendered server-side, just return null instead
+  if (!isMounted) {
+    return null;
+  }
+
+  // Render a modal component with hard-coded props
+  return (
+    <>
+      <Modal
+        title="Test Modal"
+        description="Test description."
+        isOpen
+        onChange={() => {}}
+      >
+        Test Children
+      </Modal>
+    </>
+  );
+}
+
+export default ModalProvider;
